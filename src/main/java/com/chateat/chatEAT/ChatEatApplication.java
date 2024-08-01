b6c6d5c0be7fb4f57946d70bc423453773b62d06
Kyumin-Chang <kyumin0529@naver.com>
2024-08-01 15:17:05 +0900

    feat: Init project

---
+++ src/main/java/com/chateat/chatEAT/ChatEatApplication.java
@@ -0,0 +1,13 @@
+package com.chateat.chatEAT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatEatApplication.class, args);
+	}
+
+}
