7d014db03b66688b46e4389bbc8cdfcfdcee64c8
Kyumin-Chang <kyumin0529@naver.com>
2024-08-12 02:20:41 +0900

    fix: 소셜로그인으로 인한 수정

--- src/main/java/com/chateat/chatEAT/domain/member/Member.java
+++ src/main/java/com/chateat/chatEAT/domain/member/Member.java
@@ -30,7 +30,6 @@ public class Member {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
     private String password;
 
     @Column(unique = true)
