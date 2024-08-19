946bcc57392d1389422ce9b3243394752e17b961
Kyumin-Chang <kyumin0529@naver.com>
2024-08-19 16:15:42 +0900

    chore: api admin 권한 추가

--- src/main/java/com/chateat/chatEAT/api/AdminController.java
+++ src/main/java/com/chateat/chatEAT/api/AdminController.java
@@ -4,6 +4,8 @@ import com.chateat.chatEAT.domain.member.request.AuthorizeRoleRequest;
 import com.chateat.chatEAT.domain.member.response.AuthorizeRoleResponse;
 import com.chateat.chatEAT.domain.member.response.MemberListPageResponse;
 import com.chateat.chatEAT.domain.member.service.MemberService;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,10 +13,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/admin")
@@ -24,16 +30,19 @@ public class AdminController {
     private final MemberService memberService;
 
     @PatchMapping("/change-role/{memberId}")
-    public ResponseEntity<AuthorizeRoleResponse> changeRole(@PathVariable("memberId") Long memberId, @RequestBody AuthorizeRoleRequest authorizeRoleRequest) {
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<AuthorizeRoleResponse> changeRole(@PathVariable("memberId") Long memberId,
+                                                            @RequestBody AuthorizeRoleRequest authorizeRoleRequest) {
         AuthorizeRoleResponse response = memberService.changRole(authorizeRoleRequest, memberId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/members")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Map<String, Object>> getMemberListPage(
             @RequestParam(value = "p", defaultValue = "1") int p,
             @RequestParam(value = "size", defaultValue = "20") int size) {
-        Pageable pageable = PageRequest.of(p - 1, size, Sort.by("nickname").descending());
+        Pageable pageable = PageRequest.of(p - 1, size, Sort.by("id").ascending());
         Page<MemberListPageResponse> responses = memberService.findAllMembers(pageable);
 
         Map<String, Object> response = new HashMap<>();
