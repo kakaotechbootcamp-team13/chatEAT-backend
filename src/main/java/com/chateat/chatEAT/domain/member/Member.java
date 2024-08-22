40ca32897de115923c541b31be90974013b3f4d8
Kyumin-Chang <kyumin0529@naver.com>
2024-08-22 16:18:48 +0900

    chore: OAuth2.0 회원가입시 패스워드 랜덤 생성, 패스워드 저장 필수

--- src/main/java/com/chateat/chatEAT/domain/member/Member.java
+++ src/main/java/com/chateat/chatEAT/domain/member/Member.java
@@ -1,8 +1,19 @@
 package com.chateat.chatEAT.domain.member;
 
 import com.chateat.chatEAT.oauth2.SocialType;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Table(name = "member")
@@ -18,7 +29,8 @@ public class Member {
 
     @Column(nullable = false, unique = true)
     private String email;
-
+    
+    @Column(nullable = false)
     private String password;
 
     @Column(unique = true)
