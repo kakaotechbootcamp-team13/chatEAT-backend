55c6110f3a9f5204ccb53c1ae59d17c82ad61485
PARK-SSOYEON <soyeon00307@gmail.com>
2024-08-23 14:14:40 +0900

    feat: 채팅 DB에서 불러오기 구현

--- src/main/java/com/chateat/chatEAT/domain/chat/repository/OutputChatRepository.java
+++ src/main/java/com/chateat/chatEAT/domain/chat/repository/OutputChatRepository.java
@@ -3,5 +3,8 @@ package com.chateat.chatEAT.domain.chat.repository;
 import com.chateat.chatEAT.domain.chat.OutputChat;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface OutputChatRepository extends MongoRepository<OutputChat, String> {
+    List<OutputChat> findByEmailOrderByTimestamp(String email);
 }
