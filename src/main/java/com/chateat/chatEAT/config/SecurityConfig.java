203980f51dea6456e342a5185a3f7ebdbbe66a3f
Kyumin-Chang <kyumin0529@naver.com>
2024-08-30 17:52:07 +0900

    chore: cors 정책 url 차단

--- src/main/java/com/chateat/chatEAT/config/SecurityConfig.java
+++ src/main/java/com/chateat/chatEAT/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import com.chateat.chatEAT.oauth2.CustomOAuth2MemberService;
 import com.chateat.chatEAT.oauth2.handler.OAuth2LoginFailureHandler;
 import com.chateat.chatEAT.oauth2.handler.OAuth2LoginSuccessHandler;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -94,8 +94,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:3000", frontendServer));
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://www.chateat.store"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.addExposedHeader("Authorization");
